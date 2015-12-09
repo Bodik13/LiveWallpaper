d27353efcddbb22571ff5c009ef2521bdf8e15f1
bodik13 <hbodya13@gmail.com>
2015-12-09 20:10:00 +0200

    Initial commit

---
+++ app/build/generated/source/buildConfig/release/ffvideolivewallpaper/frankandrobot/com/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ffvideolivewallpaper.frankandrobot.com;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "ffvideolivewallpaper.frankandrobot.com";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.9.0";
+}
---
+++ app/src/main/obj/local/armeabi/objs/video/video.o.d
@@ -0,0 +1,215 @@
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/obj/local/armeabi/objs/video/video.o: \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/video.c \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/GLES/gl.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/GLES/glplatform.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/_wchar_limits.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/GLES/glext.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavcodec/avcodec.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/errno.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/errno.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/errno.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm-generic/errno.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm-generic/errno-base.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavcore/samplefmt.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/avutil.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/common.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/ctype.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/inttypes.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/limits.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/math.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/string.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/strings.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/memory.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/attributes.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/avconfig.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/mem.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/avutil.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/error.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/mathematics.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/rational.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/intfloat_readwrite.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/log.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/pixfmt.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/cpu.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavcore/audioconvert.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavcore/avcore.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavformat/avformat.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/time.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/time.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/time.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/siginfo.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavformat/avio.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/common.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/log.h \
+ /home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libswscale/swscale.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/jni.h \
+ /home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/log.h
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/GLES/gl.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/GLES/glplatform.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/GLES/glext.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavcodec/avcodec.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/errno.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/errno.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/errno.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm-generic/errno.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavcore/samplefmt.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/avutil.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/common.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/ctype.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/inttypes.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/math.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/string.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/attributes.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/avconfig.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/mem.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/avutil.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/error.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/mathematics.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/rational.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/intfloat_readwrite.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/log.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/pixfmt.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/cpu.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavcore/audioconvert.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavcore/avcore.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavformat/avformat.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/time.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/sys/time.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/linux/time.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm/siginfo.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavformat/avio.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/common.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libavutil/log.h:
+
+/home/joker/AndroidStudioProjects/FFvideoLiveWallpaper-master1/app/src/main/jni/ffmpeg-android/ffmpeg/libswscale/swscale.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/home/joker/Android/android-ndk-r10e/platforms/android-8/arch-arm/usr/include/android/log.h:
